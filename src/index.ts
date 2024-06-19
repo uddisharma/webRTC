7f6d1e9ffa9c4f2553caf60c59223b1de18c30d6
uddisharma <uddibhardwaj08@gmail.com>
2024-06-19 15:57:55 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Socket } from "socket.io";
+import http from "http";
+
+import express from 'express';
+import { Server } from 'socket.io';
+import { UserManager } from "./managers/UserManger";
+import cron from 'node-cron'
+
+
+
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute');
+    // Your task code here
+});
+
+
+const app = express();
+const server = http.createServer(http);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const userManager = new UserManager();
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  userManager.addUser("randomName", socket);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    userManager.removeUser(socket.id);
+  })
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
